69a5a7831b9ef85ce9eb177b677340d8be214daa
Parisaheide <parisaheid85@gmail.com>
2022-03-09 09:04:25 +1100

    commit

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -15,7 +15,7 @@ export const Projects = [
     title: "Project 2",
     tags: ["Ruby", "PostgreSQL", "Sinatra", "Heroku", "CSS"],
     paragraph:
-      "Heaven Garden web app is built on Ruby with Sinatra Framework and PostgreSQL database for tracking your flowers caring requirements,This app will allow the user to track their garden and add all tips and tools including images,days to harvest,since watered and so on...",
+      "Heaven Garden web app is built on Ruby with Sinatra Framework and PostgreSQL database for tracking your flowers requirements, This app will allow the user to track their garden and add all tips and tools including images, days to harvest, days watered and so on...",
     codelink: "https://github.com/Parisaheid85/Heaven-Garden-Project",
     projectlink: "https://limitless-lake-63472.herokuapp.com/login",
     projectlinkText: "view Project",
@@ -26,7 +26,7 @@ export const Projects = [
     title: "Project 3",
     tags: ["React", "Konva", "CSS", "Surge"],
     paragraph:
-      "An attempt of this project is to design and customise user's Tshirt by adding texts, Photos, Stickers, drawing on the Tshirt as well as changing and resizing the colors of the Tshirt,Text and drawing.",
+      "An attempt of this project is to design and customise user's Tshirt by adding texts, Photos, Stickers, drawing on the Tshirt as well as changing and resizing the colors of the Tshirt, Text and drawing.",
     codelink: "https://github.com/Parisaheid85/DeShert-React-Frontend",
     projectlink: "https://desshert.surge.sh/",
     projectlinkText: "view Project",
